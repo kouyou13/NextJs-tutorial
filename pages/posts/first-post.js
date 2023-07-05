9bcb9118f532979543ad877968d455076f25bd9c
Koyo Kinoshita <58977709+kouyou13@users.noreply.github.com>
2023-07-05 19:55:47 +0900

    first commit

---
+++ pages/posts/first-post.js
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+import Script from 'next/script'
+import Layout from '../../components/layout'
+import Head from 'next/head'
+
+export default function FirstPost(){
+  return (
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">
+          Back to home
+        </Link>
+      </h2>
+    </Layout>
+    )
+}
